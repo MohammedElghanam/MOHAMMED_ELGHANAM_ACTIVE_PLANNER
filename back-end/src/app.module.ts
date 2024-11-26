ccb2320d85807ee9496489d0ba99d1d66e22d0cd
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-11-26 15:08:40 +0100

    feat: add resource of event and participant

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EventResolver } from './event/event.resolver';
+import { EventModule } from './event/event.module';
+import { ParticipantModule } from './participant/participant.module';
 
 @Module({
   imports: [
@@ -11,8 +14,10 @@ import { MongooseModule } from '@nestjs/mongoose';
     isGlobal: true,
   }), 
   MongooseModule.forRoot(process.env.DB_NAME),
-  AuthModule],
+  AuthModule,
+  EventModule,
+  ParticipantModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EventResolver],
 })
 export class AppModule {}
